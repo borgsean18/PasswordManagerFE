cee59e62dbcbf8b2a87fc7ce2077e293404aa236
Sean <seanborg8@gmail.com>
2024-08-29 10:16:17 +0100

    larger back button

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -3,8 +3,8 @@ import styles from './BackButton.module.css';
 
 export default function BackButton() {
   return (
-    <Link href="/" className={`absolute top-4 left-4 text-white ${styles.backButton}`}>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <Link href="/" className={`absolute top-4 left-4 text-white p-2 ${styles.backButton}`}>
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
       </svg>
     </Link>
